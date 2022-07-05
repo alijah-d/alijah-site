874e3c61baa21f7a212044fd06bd115457b16424
Alijah Mancera <alijah@wealth-park.com>
2022-07-05 13:25:44 +0800

    Add Minus

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [count, setCount] = useState(0)
   return (
-    <div>Alijah</div>
+    <div>
+      <div>
+        Alijah: {count}
+      </div>
+      <div>
+
+        <button onClick={() => {
+          setCount(count + 1)
+        }}>
+          Add
+        </button>
+
+        <button onClick={() => {
+          setCount(count - 1)
+        }}>
+          Minus
+        </button>
+
+      </div>
+    </div>
   )
 }
 
